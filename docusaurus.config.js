8fcc9dd8cc3192de724f354731d10e628f2e6f65
“Guga” <gustavo.granato@hotmail.com>
2023-10-10 09:54:48 -0300

    add

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.svg',
 
   // Set the production url of your site here
-  url: 'https://gobrax-docs.verce.app',
+  url: 'https://gobrax-docs.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/help/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
